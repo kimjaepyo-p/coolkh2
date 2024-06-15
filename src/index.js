73d60a7d933377fda41e0f7ae06fa890d9f2d6f9
kimjaepyo-p <noscent78@daum.net>
2024-06-15 14:03:06 +0900

    동적객체

--- src/index.js
+++ src/index.js
@@ -24,10 +24,52 @@ const mynaiobj = {
     google : "구글"
 
 }
+console.log(mynavi[0], mynaiobj["title"], mynaiobj.title);
+
+const mynaimulti = {
+    title : [],
+    daum : [],
+    google : []
+
+}
+
+
+const booksData = {
+    "books": [
+        {
+            "title": "The Catcher in the Rye",
+            "author": "J.D. Salinger",
+            "year_published": 1951,
+            "genres": ["Fiction", "Classics"],
+            "available": true
+        },
+        {
+            "title": "To Kill a Mockingbird",
+            "author": "Harper Lee",
+            "year_published": 1960,
+            "genres": ["Fiction", "Classics"],
+            "available": false
+        },
+        {
+            "title": "1984",
+            "author": "George Orwell",
+            "year_published": 1949,
+            "genres": ["Fiction", "Dystopian", "Science Fiction"],
+            "available": true
+        }
+    ]
+};
+
+console.log(booksData.books[2]["title"], booksData.books[2].title,
+    booksData["books"][2]["title"]
+)
+console.log(booksData.books[2].genres[0])
+console.log(booksData.books[2]["title"], booksData.books[2].title, booksData["books"][2]["title"]);
+
+console.log(booksData.books[2].genres[0]);
 
 
 
-console.log(mynavi[0], mynaiobj["naver"], mynaiobj.naver);
 
 
  
